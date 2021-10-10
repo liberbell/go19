bb9bee367d893deb7b2bae5840d32db5be72bf2e
Liberbell <liberbell@gmail.com>
2021-10-11 08:52:29 +0900

    main2

--- pj4/main.go
+++ pj4/main.go
@@ -16,7 +16,7 @@ func main() {
 	db, _ := sql.Open("mysql", "root:dbpassword!#edc@tcp(127.0.0.1:3306)/sys")
 	defer db.Close()
 
-	db.Query("CREATE TABLE PERSON (first_name varchar(50), last_name varchar(50)")
+	db.Query("CREATE TABLE PERSON (first_name varchar(50), last_name varchar(50))")
 
 	db.Query("INSERT INTO PERSON (first_name, last_name) values ('Bob', 'Baker'), ('Betty', 'White') ")
 	dataset, _ := db.Query("SELECT * FROM PERSON")
