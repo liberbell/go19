0d67cfebc2c009d12f42581be4355e6476782cc8
Liberbell <liberbell@gmail.com>
2021-10-11 23:18:24 +0900

    main4

--- pj6/main.go
+++ pj6/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusBadRequest)
+	})
+
+	http.HandleFunc("/withargs/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "This is a response from a %s request. The arguments are: args1:%s, args2:%s", r.Method, r.URL.Query().Get("arg1"), r.URL.Query().Get("arg2"))
 	})
 }
