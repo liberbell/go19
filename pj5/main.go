768e53e24f2f6dc96a04741b3a013aca743ec29a
Liberbell <liberbell@gmail.com>
2021-10-11 22:49:00 +0900

    main6

--- pj5/main.go
+++ pj5/main.go
@@ -12,4 +12,5 @@ func main() {
 	defer db.Close()
 
 	db.Query("CREATE TABLE PERSON (first_name varchar(50), last_name varchar(50))")
+	db.Query("INSERT INTO PERSON (first_name, last_name) values ('Bob', 'Barker'),('Betty', 'White')")
 }
