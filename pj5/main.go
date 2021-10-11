5cbf318403acf3c40b7d4a6a06c3caf1f76a882e
Liberbell <liberbell@gmail.com>
2021-10-11 22:50:06 +0900

    main7

--- pj5/main.go
+++ pj5/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/jmoiron/sqlx"
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type Person struct {
 	FirstName string `db:"first_name"`
@@ -13,4 +17,9 @@ func main() {
 
 	db.Query("CREATE TABLE PERSON (first_name varchar(50), last_name varchar(50))")
 	db.Query("INSERT INTO PERSON (first_name, last_name) values ('Bob', 'Barker'),('Betty', 'White')")
+
+	table_rec := []Person{}
+	db.Select(&table_rec, "SELECT * FROM PERSON")
+
+	fmt.Println(table_rec)
 }
