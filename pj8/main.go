02c2791511de55287e38da307395c6b1af541b7d
Liberbell <liberbell@gmail.com>
2021-10-12 08:48:10 +0900

    main7

--- pj8/main.go
+++ pj8/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Person struct {
-	FirstName string `json:"fname"`
-	LastName  string `json:"lname"`
-	Hobbies   string `json:"hobbies"`
+	FirstName string   `json:"fname"`
+	LastName  string   `json:"lname"`
+	Hobbies   []string `json:"hobbies"`
 }
 
 func main() {
