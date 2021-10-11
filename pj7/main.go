b1debd039420732a45b04a132ed287bc9420fed0
Liberbell <liberbell@gmail.com>
2021-10-12 08:31:23 +0900

    main2

--- pj7/main.go
+++ pj7/main.go
@@ -1,8 +1,9 @@
 package main
 
 type Person struct {
-	FirstName string `xml:"fname"`
-	LastName  string `xml:"lname"`
+	FirstName string   `xml:"fname"`
+	LastName  string   `xml:"lname"`
+	Hobbies   []string `xml:hobbies`
 }
 
 func main() {
