53bb8bb84b8d46a085095c50eb14ff8187e551bd
Liberbell <liberbell@gmail.com>
2021-10-13 08:32:36 +0900

    main2

--- pj9/main.go
+++ pj9/main.go
@@ -14,17 +14,25 @@ type Person struct {
 }
 
 func main() {
-	var People []Person
+	var people []Person
 	http.HandleFunc("/person/", func(rw http.ResponseWriter, r *http.Request) {
 
 		switch r.Method {
 		case http.MethodGet:
-			jsonRequest, _ := json.MarshalIndent(&People, " ", "   ")
+			jsonRequest, _ := json.MarshalIndent(&people, " ", "   ")
 			fmt.Fprint(rw, string(jsonRequest))
 
 		case http.MethodPost:
 			requestBodyBytes, _ := ioutil.ReadAll(r.Body)
 			var newPerson Person
+			json.Unmarshal(requestBodyBytes, &newPerson)
+
+			for i := range people {
+				if people[i].ID == newPerson.ID {
+					rw.WriteHeader(http.StatusConflict)
+					return
+				}
+			}
 		}
 	})
 }
